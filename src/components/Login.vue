028a9fde94f12cd3edb476d51c01b5a005905a65
Carlo Beltrame <beltrame@puzzle.ch>
2018-10-24 16:09:26 +0200

    Autofocus phone field on login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,7 +3,7 @@
     <header class="title has-text-centered column is-full is-one-third-desktop is-offset-one-third-desktop">Login</header>
     <div class="box column is-full is-one-third-desktop is-offset-one-third-desktop">
       <form v-on:submit.prevent="login">
-        <b-field label="Händynummärä"><b-input type="tel" pattern="((\+41\s?)?|(0041\s?)?|0)7[6-9]\s?\d{3}\s?\d{2}\s?\d{2}" placeholder="079 het sie gseit" v-model="phone"/></b-field>
+        <b-field label="Händynummärä"><b-input type="tel" pattern="((\+41\s?)?|(0041\s?)?|0)7[6-9]\s?\d{3}\s?\d{2}\s?\d{2}" placeholder="079 het sie gseit" v-model="phone" autofocus/></b-field>
         <b-field label="Gruppänamä">
           <b-autocomplete v-if="!userIsAlreadyRegistered" v-model="groupName" open-on-focus :data="groups" field="name"/>
           <b-input v-else v-model="specifiedUser.groupName" disabled />
