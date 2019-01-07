319d46df67fc5f28072d5748423e3cc4091b2d5b
Carlo Beltrame <carlo@beltra.me>
2019-01-07 16:07:32 +0100

    Simplify station owner calculation to a more functional form

--- src/business.js
+++ src/business.js
@@ -6,23 +6,9 @@ export function groupSaldo (groupId, settings, stationVisits) {
 
 function stationExpenses (groupId, stationVisits) {
   if (!stationVisits) return 0
-  return uniqueByStation(stationVisits.filter(visit => ownedBy(visit.station.id, groupId, stationVisits)))
-    .reduce((total, visit) => total - visit.station.value, 0)
+  return firstVisitsByStation(stationVisits).filter(visit => visit.group.id === groupId).reduce((total, visit) => total - visit.station.value, 0)
 }
 
-function ownedBy (stationId, groupId, stationVisits) {
-  var firstVisit = stationVisits.find(visit => visit.station.id === stationId)
-  return firstVisit && firstVisit.group.id === groupId
-}
-
-function uniqueByStation (stationVisits) {
-  var unique = {}
-  var distinct = []
-  stationVisits.forEach(visit => {
-    if (unique[visit.station.id] === undefined) {
-      distinct.push(visit)
-      unique[visit.station.id] = 0
-    }
-  })
-  return distinct
+function firstVisitsByStation (stationVisits) {
+  return Array.from(stationVisits.reduceRight((map, visit) => map.set(visit.station.id, visit), new Map()).values())
 }
