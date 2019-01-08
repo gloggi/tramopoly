04c00b6956f41df0e4091480dcbf6a7fbfd19831
Carlo Beltrame <carlo@beltra.me>
2019-01-08 15:47:19 +0100

    Calculate interest and add it to group saldo

--- src/business.js
+++ src/business.js
@@ -1,18 +1,22 @@
 
-export function groupSaldo (groupId, settings, stationVisits) {
+export function groupSaldo (groupId, settings, stationVisits, now = new Date()) {
   if (!settings) return 0
   return settings.starterCash +
-    stationExpenses(groupId, stationVisits, settings)
+    stationExpenses(groupId, stationVisits, settings, now)
 }
 
-function stationExpenses (groupId, stationVisits, settings) {
+export function stationOwners (stationVisits, now = new Date()) {
+  return filter(stationVisits, now).reduceRight((map, visit) => map.set(visit.station.id, visit.group.id), new Map())
+}
+
+function stationExpenses (groupId, stationVisits, settings, now) {
   if (!stationVisits || !settings) return 0
-  let buyingVisits = firstVisitsByStation(stationVisits).filter(visit => visit.group.id === groupId)
+  let visits = filter(stationVisits, now)
+  let buyingVisits = firstVisitsByStation(visits).filter(visit => visit.group.id === groupId)
   let boughtStationIds = buyingVisits.map(visit => visit.station.id)
-  let payingVisits = stationVisits.filter(visit => visit.group.id === groupId).filter(visit => !boughtStationIds.includes(visit.station.id))
-  let payeeVisits = stationVisits.filter(visit => visit.group.id !== groupId).filter(visit => boughtStationIds.includes(visit.station.id))
-  console.log(buyingVisits, payingVisits, payeeVisits)
-  return -buyingCost(buyingVisits) - rentAmount(payingVisits, settings.rentRate) + rentAmount(payeeVisits, settings.rentRate)
+  let payingVisits = visits.filter(visit => visit.group.id === groupId).filter(visit => !boughtStationIds.includes(visit.station.id))
+  let payeeVisits = visits.filter(visit => visit.group.id !== groupId).filter(visit => boughtStationIds.includes(visit.station.id))
+  return -buyingCost(buyingVisits) - rentAmount(payingVisits, settings.rentRate) + rentAmount(payeeVisits, settings.rentRate) + interestAmount(buyingVisits, settings.interestPeriod, settings.interestRate, settings.gameEnd, now)
 }
 
 function firstVisitsByStation (stationVisits) {
@@ -27,6 +31,10 @@ function rentAmount (visits, rentRate) {
   return visits.reduce((total, visit) => total + visit.station.value * rentRate, 0)
 }
 
-export function stationOwners (stationVisits) {
-  return stationVisits.reduceRight((map, visit) => map.set(visit.station.id, visit.group.id), new Map())
+function interestAmount (visits, period, rate, gameEnd, now) {
+  return Math.round(visits.map(visit => (Math.min(now, gameEnd.toDate()) - visit.time.toDate()) / 60000.0 / period * rate * visit.station.value).reduce((sum, stationInterest) => sum + stationInterest, 0.0))
+}
+
+function filter (stationVisits, now) {
+  return stationVisits.filter(visit => visit.time.toDate() <= now)
 }
