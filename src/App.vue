305268e9a9243be4cc6a312fe648522cabdb2cff
Carlo Beltrame <carlo@beltra.me>
2023-01-03 23:43:27 +0100

    Clean up scaffolds and POC supabase connection and login

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,80 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router"
-import HelloWorld from "./components/HelloWorld.vue"
-</script>
-
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+    <div v-if="!signedInUser">Hello world!</div>
+    <div v-else>Hello {{ signedInUser.user_metadata.full_name }}</div>
+    <input name="title" v-model="title" />
+    <input name="content" v-model="content" />
+    <button @click="createPost">Save</button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+      <button v-if="!signedInUser" @click="signInWithKeycloak">Sign in</button>
+      <button v-else @click="signOut">Sign out</button>
+    </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div v-for="(post, index) in posts" :key="index">
+      <h3>{{ post.title }}</h3>
+      <article>{{ post.content }}</article>
     </div>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+import { RouterView } from 'vue-router'
+import { supabase } from '@/client'
+
+export default {
+  name: 'App',
+  components: { RouterView },
+  data: () => ({
+    posts: [],
+    title: '',
+    content: '',
+    signedInUser: null,
+  }),
+  mounted() {
+    this.fetchPosts()
+    supabase.auth.getUser().then(({ data: { user } }) => {
+      this.signedInUser = user
+      console.log(user)
+    })
+  },
+  methods: {
+    async fetchPosts() {
+      const { data } = await supabase.from('posts').select()
+      this.posts = data
+    },
+    async createPost() {
+      await supabase
+        .from('posts')
+        .insert([
+          {
+            title: this.title,
+            content: this.content,
+          },
+        ])
+        .single()
+      this.title = ''
+      this.content = ''
+      this.fetchPosts()
+    },
+    async signInWithKeycloak() {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'keycloak',
+        options: {
+          scopes: 'openid',
+        },
+      })
+      console.log(data, error)
+    },
+    async signOut() {
+      const { error } = await supabase.auth.signOut()
+      console.log(error)
+      this.signedInUser = null
+    },
+  },
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,11 +1,9 @@
-import { describe, it, expect } from "vitest";
+import { describe, it } from 'vitest'
 
-import { mount } from "@vue/test-utils";
-import HelloWorld from "../HelloWorld.vue";
-
-describe("HelloWorld", () => {
-  it("renders properly", () => {
-    const wrapper = mount(HelloWorld, { props: { msg: "Hello Vitest" } });
-    expect(wrapper.text()).toContain("Hello Vitest");
-  });
-});
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+    // TODO use vue testing library for any component tests
+    //const wrapper = mount(HelloWorld, { props: { msg: "Hello Vitest" } })
+    //expect(wrapper.text()).toContain("Hello Vitest")
+  })
+})
